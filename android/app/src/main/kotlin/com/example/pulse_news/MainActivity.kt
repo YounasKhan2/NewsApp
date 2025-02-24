55cd02a179e99fe6391e4d003ce2d9e91f21e6de
YounasKhan2 <162640959+YounasKhan2@users.noreply.github.com>
2025-02-24 07:19:26 -0800

    Debugging Gradels

--- android/app/src/main/kotlin/com/example/pulse_news/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pulse_news
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/pulsenews/app/pulse_news/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pulsenews.app.pulse_news
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
